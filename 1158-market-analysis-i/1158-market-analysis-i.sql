6d60d614fff796e4df57995dcdf96fe9d8ed831d
Dorito <80398645+baksohyeon@users.noreply.github.com>
2022-11-10 12:15:58 +0900

    Time: 2246 ms (15.21%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT u.user_id as buyer_id, u.join_date, COUNT(o.order_date) as orders_in_2019
+FROM Users as u
+LEFT JOIN Orders as o
+ON u.user_id = o.buyer_id AND Year(o.order_date) = '2019'
+GROUP BY u.user_id
+ORDER BY buyer_id asc 
+
+
+# 유저를 찾아라, 날짜 조인하고 2019년에 샀던 사람중에 주문 수를 구해라, 순서는 자유
+
+# SELECT u.user_id AS buyer_id, join_date, COUNT(order_date) AS orders_in_2019 
+# FROM Users as u
+# LEFT JOIN Orders as o
+# ON u.user_id = o.buyer_id AND YEAR(order_date) = '2019'
+# GROUP BY u.user_id
