d16919796553135f4df52269317134fddbe78077
Dorito <80398645+baksohyeon@users.noreply.github.com>
2022-11-10 12:17:33 +0900

    Time: 2139 ms (19.52%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -3,7 +3,7 @@
 SELECT u.user_id as buyer_id, u.join_date, COUNT(o.order_date) as orders_in_2019
 FROM Users as u
 LEFT JOIN Orders as o
-ON u.user_id = o.buyer_id AND Year(o.order_date) = '2019'
+ON u.user_id = o.buyer_id AND Year(o.order_date) = 2019
 GROUP BY u.user_id
 ORDER BY buyer_id asc 
 
