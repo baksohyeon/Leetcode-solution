687617db0e9e068e1740113a7a9f2c7fad6dc19d
Dorito <80398645+baksohyeon@users.noreply.github.com>
2022-11-11 12:21:08 +0900

    Time: 425 ms (79.49%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -2,14 +2,14 @@
 
 
 # Runtime: 374 ms, faster than 98.67% of MySQL online submissions for Actors and Directors Who Cooperated At Least Three Times.
-# SELECT actor_id, director_id
-# FROM ActorDirector
-# GROUP BY actor_id, director_id
-# HAVING COUNT(1) > 2
-
-
-
 SELECT actor_id, director_id
 FROM ActorDirector
 GROUP BY actor_id, director_id
-HAVING COUNT(*) > 2
+HAVING COUNT(1) > 2
+
+
+# Runtime: 424 ms, faster than 79.95% of MySQL online submissions for Actors and Directors Who Cooperated At Least Three Times.
+# SELECT actor_id, director_id
+# FROM ActorDirector
+# GROUP BY actor_id, director_id
+# HAVING COUNT(*) > 2
