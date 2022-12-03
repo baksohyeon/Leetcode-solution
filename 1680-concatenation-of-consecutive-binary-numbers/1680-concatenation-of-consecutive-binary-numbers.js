f56ff6adc93f42b25fb1e62f6ed86904a49c0acf
Dorito <80398645+baksohyeon@users.noreply.github.com>
2022-12-03 21:50:58 +0900

    Time: 1922 ms (29.17%), Space: 64.8 MB (8.33%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const concatenatedBinary = (n) => {
+  const nums = [...Array(n).keys()].map((x) => x + 1); 
+
+  return nums.reduce((acc, num) => {
+    return (acc = acc * (1 << num.toString(2).length) + num) % (1e9 + 7);
+  }, 0);
+
+};
