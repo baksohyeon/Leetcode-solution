2acf3560b480fb35499c2c8be6b2a09d44f27422
Dorito <80398645+baksohyeon@users.noreply.github.com>
2023-03-08 11:47:48 +0900

    Time: 125 ms (14.75%), Space: 46.1 MB (11.48%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+const minEatingSpeed = (piles, h) => {
+  // k [consumed banana/hr]
+  const sortedBananaPiles = piles.sort((a, b) => a - b);
+  let start = 0;
+  let end = sortedBananaPiles[sortedBananaPiles.length - 1];
+
+  while (start <= end) {
+    const mid = start + Math.floor((end - start) / 2);
+    const expectedEatingHour = piles.reduce((acc, cur) => {
+      return acc + Math.ceil(cur / mid);
+    }, 0);
+    if (expectedEatingHour > h) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return start;
+};
