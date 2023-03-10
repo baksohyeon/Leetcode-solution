e0b10af52e89267396453229c0b686440d431214
Dorito <80398645+baksohyeon@users.noreply.github.com>
2023-03-10 12:02:58 +0900

    Time: 194 ms (32.76%), Space: 48.2 MB (24.14%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.js
@@ -1,15 +1,15 @@
 const countVowelSubstrings = (word) => {
   const arrayOfVowel = ["a", "e", "i", "o", "u"];
-  const vowelMap = new Set(arrayOfVowel);
+  const vowelSet = new Set(arrayOfVowel);
   const chars = [...word];
   let count = 0;
 
   for (let i = 0; i < word.length - 4; i++) {
-    if (!vowelMap.has(chars[i])) {
+    if (!vowelSet.has(chars[i])) {
       continue;
     }
     for (let j = i + 1; j < chars.length; j++) {
-      if (!vowelMap.has(chars[j])) {
+      if (!vowelSet.has(chars[j])) {
         break;
       } else if (j >= i + 4) {
         const subString = chars.slice(i, j + 1);
