c45462e56325b98e850c01eaf536bc393645671e
Dorito <80398645+baksohyeon@users.noreply.github.com>
2023-03-13 13:10:54 +0900

    Time: 84 ms (48.83%), Space: 50.9 MB (6.45%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
@@ -17,27 +17,52 @@
  * @param {ListNode} head
  * @return {TreeNode}
  */
-const sortedListToBST = (head) => {
-  let cur = head;
-  let count = 0;
-
-  while (cur) {
-    cur = cur.next;
-    count++;
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
-  const treeify = (i, j) => {
-    if (j < i) {
-      return null;
-    }
-    let mid = (i + j) >> 1;
-    let node = new TreeNode();
-    node.left = treeify(i, mid - 1);
+}
 
-    node.val = cur.val;
-    cur = cur.next;
-    node.right = treeify(mid + 1, j);
-    return node;
-  };
-  cur = head;
-  return treeify(1, count);
+function generateRootAndBranch(array) {
+  // 배열이 비었을 경우 재귀 멈춤
+  if (!array.length) return null;
+
+  // 가운데 값 구함
+  const mid = Math.round((array.length - 1) / 2);
+
+  // 트리 노드로 만듬
+  const root = new TreeNode(array[mid]);
+
+  // 왼쪽 분리
+  const leftArray = array.slice(0, mid);
+  if (leftArray.length) {
+    // 왼쪽 그룹끼리 재귀 잘 되길 기도
+    root.left = generateRootAndBranch(leftArray);
+  }
+
+  // 오른쪽 분리
+  const rightArray = array.slice(mid + 1);
+  if (rightArray.length) {
+    // 오른쪽 그룹끼리 재귀 잘 되길 기도
+    root.right = generateRootAndBranch(rightArray);
+  }
+  return root;
+}
+
+function convertListToArray(head) {
+  const array = [];
+  while (head) {
+    array.push(head.val);
+    head = head.next;
+  }
+  return array;
+}
+
+const sortedListToBST = (head) => {
+  if (!head) return null;
+  const array = convertListToArray(head);
+  const root = generateRootAndBranch(array);
+  return root;
 };
